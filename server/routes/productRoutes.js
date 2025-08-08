e75d1c34010c4027226597af25f19be8fa33d1c2
bante27 <mitikubantalem@gmail.com>
2025-08-08 07:45:23 -0400

    error fix

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Product = require('../models/product');
-const User = require('../models/user');
+const User = require('../models/User');
 const multer = require('multer');
 const cloudinary = require('cloudinary').v2;
 
